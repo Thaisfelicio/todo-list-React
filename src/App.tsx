127e706c45040521bce7ebd8ff83c33d8b413b27
Thaisfelicio <thsfelicio@gmail.com>
2026-02-02 00:11:32 -0300

    feat: Add badge and button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import Text from "./components/text";
 import TrashIcon from "./assets/icons/trash.svg?react";
+import PlusIcon from "./assets/icons/plus.svg?react";
+import Icon from "./components/icon";
+import Badge from "./components/badge";
+import Button from "./components/button";
 
 export default function App() {
   
@@ -8,6 +12,12 @@ export default function App() {
     <div className="grid gap-3">
       <Text>Olá mundo</Text>
       <Icon svg={TrashIcon} className="fill-green-base"/>
+      <div>
+        <Badge variant="primary">2</Badge>
+      </div>
+      <div>
+        <Button icon={PlusIcon}>Nova tarefa</Button>
+      </div>
     </div>
     
   )
